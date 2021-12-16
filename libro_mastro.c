93d920aeace954a737345271c2539b68f9776f1f
Francesco <frvncesco20@gmail.com>
2021-12-16 17:56:49 +0100

    Creazione del listener di libro mastro ed allocazione della matrice di transazioni

--- libro_mastro.c
+++ libro_mastro.c
@@ -1,17 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 #include <pthread.h>
 #include "config.h"
 
+void masterBookListener(transactions, i)
+{
+        //ascolta le richieste provenienti dai nodi
+}
 
-void *masterBookStart(void *treadId)
+void* masterBookStart(void *treadId)
 {
         printf("Test creazione libro mastro\n");
 
+        TransactionMaster*** transactions = (TransactionMaster***)malloc(sizeof(TransactionMaster**) * SO_REGISTRY_SIZE);
         int i = 0;
         while(SO_REGISTRY_SIZE > i)
         {
-
+                transactions[i] = (TransactionMaster**)malloc(sizeof(TransactionMaster*) * SO_BLOCK_SIZE);
+                masterBookListener(transactions, i);
                 i++;
         }
         return (void*) 1;
