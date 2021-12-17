fed2e69aa818849f44cb7ed1ca3e8b89fc57ce1d
Francesco <frvncesco20@gmail.com>
2021-12-17 12:48:42 +0100

    Gestione del SO_RETRY

--- userProcess.c
+++ userProcess.c
@@ -7,7 +7,7 @@
 
 int calcBalance(void *treadId)
 {
-
+        //leggo tutto il libro mastro(ciclo) e prendo tutte le qty delle transazioni
 }
 
 pthread_t findReceiver()
@@ -34,16 +34,25 @@ Transaction* createTransaction(void *treadId, int balance)
 
 void *userStart(void *treadId)
 {
-        //creazione transazione
-        //sender = threadId
         printf("Test creazione processo utente\n");
-        int balance = calcBalance(treadId);
-        if(balance >= 2)
+        int actual_retry = 0;
+        while(actual_retry <= SO_RETRY)
         {
-                createTransaction(treadId, balance);
+                int balance = calcBalance(treadId);
+                if(balance >= 2)
+                {
+                        Transaction* t = createTransaction(treadId, balance);
+                        //sceglie il node
+                        //invia la transazione al node
+                        //va in wait
+                        //se l'esito è negativo actual_retry va incrementato e va ripetuta la stessa transazione
+                }
+                else
+                {
+                        //gestione del caso < 2
+                        //va in wait e il ciclo riparte da capo
+                }
         }
-        printf("aaa\n");
-        //gestione del caso < 2
-        //gestione del retry
+        //se ci si trova qui allora il processo per SO_RETRY volte non è riuscito a portare a termine la transazione -> deve terminare
         pthread_exit(NULL);
 }
